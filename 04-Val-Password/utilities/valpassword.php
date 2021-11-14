bba19eeb068e41b0c459eb7e2d0f851f2ef5fac8
Hadsonpar <hadson1@gmail.com>
2021-11-14 02:14:42 -0500

    Función PHP - Validar password

---
+++ 04-Val-Password/utilities/valpassword.php
@@ -0,0 +1,26 @@
+<?php
+    function validar_clave($clave,&$error_clave){
+        if(strlen($clave) < 6){
+           $error_clave = "La clave debe tener al menos 6 caracteres";
+           return false;
+        }
+        if(strlen($clave) > 8){
+           $error_clave = "La clave no puede tener más de 8 caracteres";
+           return false;
+        }
+        if (!preg_match('`[a-z]`',$clave)){
+           $error_clave = "La clave debe tener al menos una letra minúscula";
+           return false;
+        }
+        if (!preg_match('`[A-Z]`',$clave)){
+           $error_clave = "La clave debe tener al menos una letra mayúscula";
+           return false;
+        }
+        if (!preg_match('`[0-9]`',$clave)){
+           $error_clave = "La clave debe tener al menos un carácter numérico";
+           return false;
+        }
+        $error_clave = "";
+        return true;
+    } 
+?>
