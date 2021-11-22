128250a996307d0212fcb49bbb9b838e1e1888ce
Hadsonpar <hadson1@gmail.com>
2021-11-22 01:35:27 -0500

    Funcion utilitario para  validar password

--- 04-Val-Password/index.php
+++ 04-Val-Password/index.php
@@ -1,9 +1,6 @@
 <?php
 require_once('utilities/valpassword.php');
-<<<<<<< HEAD
 
-=======
->>>>>>> bba19eeb068e41b0c459eb7e2d0f851f2ef5fac8
 if ($_POST){
    $error_encontrado="";
    if (validar_clave($_POST["clave"], $error_encontrado)){
@@ -14,17 +11,10 @@ if ($_POST){
       echo "<div class='col-4 alert alert-danger' role='alert'>
             PASSWORD NO VÁLIDO ". $error_encontrado.
             "</div>";
-<<<<<<< HEAD
-
    }
 }
 ?>
 
-=======
-   }
-}
-?>
->>>>>>> bba19eeb068e41b0c459eb7e2d0f851f2ef5fac8
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -34,7 +24,6 @@ if ($_POST){
     <link href="css/style.css" rel="stylesheet">
     <link href="css/bootstrap.min.css" rel="stylesheet">
 </head>
-<<<<<<< HEAD
 
 <body>
     <form action="index.php" method="post">        
@@ -44,16 +33,5 @@ if ($_POST){
                 <input type="submit" value="Enviar">     
                 
     </form>
-
-=======
-<body>
-    <form action="index.php" method="post">        
-                
-        Escribe un Password:
-        <input type=password name="clave">
-        <input type="submit" value="Enviar">     
-                
-    </form>
->>>>>>> bba19eeb068e41b0c459eb7e2d0f851f2ef5fac8
 </body>
 </html>
